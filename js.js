39a653a19811a09938a273e10cf0b69239c0e195
7Pitry <milo.pietrz@icloud.com>
2026-08-05 22:18:29 +0200

    Everything done

--- js.js
+++ js.js
@@ -1,30 +1,59 @@
 let human = 0;
-let humanChoice = 0;
+let humanFixed = 0;
 let humanScore = 0;
 let compScore = 0;
 
 function getComputerChoice(){
-    random_number = Math.floor(Math.random() * 3);
+    let random_number = Math.floor(Math.random() * 3);
     if(random_number === 0){
-        return console.log("rock");
+        return "rock";
     }else if(random_number === 1){
-        return console.log("paper");
+        return "paper";
     }else{
-        return console.log("scisors");
+        return "scissors";
     }
 }
 
 function getHumanChoice(){
     do{
     human = prompt("podaj znak")
-    humanChoice= human.toLowerCase()
-    if(humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scisors"){
+    humanFixed= human.toLowerCase()
+    if(humanFixed !== "rock" && humanFixed !== "paper" && humanFixed !== "scissors"){
         console.log("zle wprowadzony znak")
     }
-    }while(humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scisors")
-        return humanChoice
+    }while(humanFixed !== "rock" && humanFixed !== "paper" && humanFixed !== "scissors")
+        return humanFixed
 }
 
-    
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+function playRound(){
+    let computerChoice = getComputerChoice()
+    let humanChoice = getHumanChoice()
+    console.log(computerChoice)
+    if(humanChoice === computerChoice){
+        console.log("Draw")
+    }else if((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "rock")){
+        console.log("You win!")
+        humanScore += 1
+    }else{
+        console.log("You lose!")
+        compScore += 1
+    }
+    console.log("=========")
+    console.log(`Your score: ${humanScore}`)
+    console.log(`Computer score: ${compScore}`)
+    console.log("=========")
+}
+
+function playGame(){
+    for(let i = 0; i < 3; i++){
+        playRound()
+    }
+    if(humanScore>compScore){
+        console.log("You win whole game!!!")
+    }else if(humanScore === compScore){
+        console.log("Mega draw!!!")
+    }else{
+        console.log("You totally lose!!!")
+    }
+}
+playGame()
